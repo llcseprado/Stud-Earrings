e295a7f583582f491c5789a9e5eecbf7d6f88ce6
llcseprado <91046919+llcseprado@users.noreply.github.com>
2022-10-24 18:50:43 +0800

    initial

---
+++ server.js
@@ -0,0 +1,15 @@
+var express =  require('express');
+var app = express();
+app.use(express.static('assets'))
+
+const port= process.env.PORT || 3000;
+app.listen(port,);
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res) =>{
+    console.log('Here')
+    res.render("home.ejs")
+})
+
+
+
