ff3f914c66c8f26ed9723175fb7fc34ec7fde36d
llcseprado <91046919+llcseprado@users.noreply.github.com>
2022-10-28 14:12:59 +0800

    3rd

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express =  require('express');
 var app = express();
+// var bodyParser = require('body-parser')
 app.use(express.static('assets'))
 
 const port= process.env.PORT || 3000;
@@ -7,6 +8,7 @@ app.listen(port,);
 
 
 app.set('view engine', 'ejs');
+// app.use(express.urlencoded({extended: true}));
 
 var fs = require("firebase-admin");
 let serviceAccount;
@@ -31,19 +33,56 @@ app.get('/', async function (req,res) {
     //gets the collection lang jd
     const itemz = await itemsCol.get();
     // const photo = await itemsCol.where('photoUrl', '==', true).get();
-
-    itemz.forEach(doc =>{
-            console.log(doc.id, '=>', doc.data());
     
+    //display sa console tanan sulod sa db 
+    itemz.forEach(doc =>{
+        console.log(doc.id, '=>', doc.data());
     });
    
     let data = {
         url: req.url,
         itemData: itemz.docs,
     }
-    // console.log(data.itemData[3]);
+    // console.log(data.itemData[0]);
     res.render("home.ejs", data);
-})
+});
 
+// app.get('/item', async function (req,res){
+//         //gets the collection lang jd
+//         const itemz = await itemsCol.get();
+//         // const photo = await itemsCol.where('photoUrl', '==', true).get();
+        
+//         //display sa console tanan sulod sa db 
+//         itemz.forEach(doc =>{
+//             console.log(doc.id, '=>', doc.data());
+//         });
+       
+//         let data = {
+//             url: req.url,
+//             itemData: itemz.docs,
+//         }
+//     res.render("item.ejs", data);
+// });
 
+app.get('/item/:itemid', async function (req, res) {
+    try {
+        console.log(req.params.itemid);
+
+    } catch (e) {
+    }
+    const item_id = req.params.itemid;
+    const item_ref = itemsCol.doc(item_id);
+    const doc = await item_ref.get();
+    if (!doc.exists) {
+        console.log('No such document!');
+    } else {
+        console.log('Document data:', doc.data());
+    }
+    // const items = await ingColl.get();
+    let data = {
+        url: req.url,
+        itemData: doc.data(),
+    }
+    res.render('item.ejs', data);
+});
 
